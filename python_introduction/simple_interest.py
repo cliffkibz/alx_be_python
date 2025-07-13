5e2fd22cf3770deb7be5b4e4077a196743de862e
cliffkibz <wyclifkibz@gmail.com>
2025-07-13 12:55:12 -0700

    Added simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+# Variables
+principal = 1000      # $1 000
+rate      = 0.05      # 5 % per year
+time      = 3         # 3 years
+
+# Simple‑interest formula
+interest = principal * rate * time
+
+# Output
+print(f"The simple interest is: {interest}")
+
